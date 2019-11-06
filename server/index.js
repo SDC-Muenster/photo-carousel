95e8f7f4fd359109eb6ffb8cd64406effda54800
Jenny Shamoo <jenny.shamoo@gmail.com>
2019-11-05 23:18:06 -0800

    update the naming

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const handler = require('./handler');
+const controller = require('./controller');
 
 const Console = console;
 
@@ -14,10 +14,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // API
-app.post('/homes', handler.Create);
-app.get('/homes', handler.Read);
-app.put('/homes', handler.Update);
-app.delete('/homes', handler.Delete);
+app.post('/homes', controller.Create);
+app.get('/homes', controller.Read);
+app.put('/homes', controller.Update);
+app.delete('/homes', controller.Delete);
 
 app.listen(port, () => {
   Console.log(`Listening on port ${port}`);
