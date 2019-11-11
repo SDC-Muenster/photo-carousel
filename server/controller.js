474570459faf6d5f7d1c705f85c9c91df16a0f73
Jenny Shamoo <jenny.shamoo@gmail.com>
2019-11-10 16:52:10 -0800

    update crud controller

--- server/controller.js
+++ server/controller.js
@@ -5,22 +5,27 @@ const Console = console;
 
 const Create = (req, res) => {
   Console.log(req.body);
-  db.HomeSet.create(req.body)
-    .then(() => res.sendStatus(201))
+  const queryArgs = [req.body.id, req.body.title, req.body.location, req.body.cost, req.body.rating, req.body.reviews, req.body.type, req.body.favorite, req.body.similarhomes, req.body.photos];
+  const queryStr = 'INSERT INTO homes VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)';
+  db.query(queryStr, queryArgs)
+    .then (() => {
+      // console.log('create success');
+      res.sendStatus(201);
+    })
     .catch((err) => {
-      console.error(err);
+      console.log('create fail', err);
       res.sendStatus(500);
     });
 };
 
-// update read controller
+
 const Read = (req, res) => {
   const { id } = req.query;
   const queryStr = 'SELECT similarhomes FROM homes WHERE id = ($1)';
 
   db.query(queryStr, [id])
     .then((data) => {
-      console.log(data.rows[0].similarhomes);
+      // console.log(data.rows[0].similarhomes);
       const similarHomesIds = data.rows[0].similarhomes;
       const homes = [];
       const promises = similarHomesIds.map((similarHomeId) => (
@@ -29,13 +34,13 @@ const Read = (req, res) => {
             homes.push(home.rows[0]);
           })
           .catch((err) => {
-            console.error(err);
+            console.error('read fail', err);
             res.sendStatus(500);
           })
       ));
       Promise.all(promises)
         .then(() => {
-          console.log('>>>', homes);
+          // console.log('>>>', homes);
           res.json(homes);
         })
         .catch((err) => console.error(err));
@@ -47,19 +52,30 @@ const Read = (req, res) => {
 };
 
 const Update = (req, res) => {
-  db.HomeSet.findOneAndUpdate({ home_id: req.query.id }, req.body)
-    .then(() => res.sendStatus(202))
+  const queryArgs = [req.body.id, req.body.title, req.body.location, req.body.cost, req.body.rating, req.body.reviews, req.body.type, req.body.favorite, req.body.similarhomes, req.body.photos];
+  const queryStr = 'UPDATE homes SET title = $2, location = $3, cost = $4, rating = $5, reviews = $6, type = $7, favorite = $8, similarhomes = $9, photos = $10 WHERE id = $1';
+  db.query(queryStr, queryArgs)
+    .then((result) => {
+      console.log('update success', result.rowCount);
+      res.sendStatus(202);
+    })
     .catch((err) => {
-      console.error(err);
+      console.log('update fail', err);
       res.sendStatus(500);
     });
 };
 
 const Delete = (req, res) => {
-  db.HomeSet.findOneAndDelete({ home_id: req.query.id })
-    .then(() => res.sendStatus(202))
+  const queryArgs = [req.query.id];
+  // console.log(req.body.id);
+  const queryStr = 'DELETE FROM homes WHERE id = $1';
+  db.query(queryStr, queryArgs)
+    .then((result) => {
+      console.log('delete success', result.rowCount);
+      res.sendStatus(202);
+    })
     .catch((err) => {
-      console.error(err);
+      console.log('delete fail', err);
       res.sendStatus(500);
     });
 };
