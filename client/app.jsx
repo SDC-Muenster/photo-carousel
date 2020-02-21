353f4e1f83827c6b629cb87914e4a237353e390c
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-02-20 22:00:25 -0800

    Separate reactDOM render from app.jsx to index.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import axios from 'axios';
 import MainCarousel from './components/MainCarousel.jsx'
 import './styles/style.css';
@@ -51,7 +50,4 @@ class App extends React.Component {
   };
 };
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('more')
-);
+export default App;
